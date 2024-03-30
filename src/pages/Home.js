a4a47d29032242ec5bedfc822729fa84d907ac09
Prinkal1 <mahajanprinkal@gmail.com>
2024-03-30 12:13:57 +0530

    home page added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,30 @@
 import React from 'react'
+import {Link} from "react-router-dom"
 
-const Home = () =>{
-    return(
-        <div>
-            
-        </div>
-    )
+const Home = ({isLoggedIn}) => {
+  return (
+    <div className='flex flex-col items-center text-white text-6xl h-full gap-y-16'>
+      <div className='pt-[13rem] '>Welcome To GNDEC Mess Mate</div>
+      <div className='flex justify-center  gap-x-16 '>
+            { !isLoggedIn &&
+                <Link to="/login">
+                    <button className='bg-richblack-800 text-richblack-100 py-[18px] 
+                    px-[28px] rounded-[18px] border border-white '>
+                        Log in
+                    </button>
+                </Link>
+            }
+            { !isLoggedIn &&
+                <Link to="/signup">
+                    <button  className='bg-richblack-800 text-richblack-100 py-[18px] 
+                    px-[18px] rounded-[18px] border  border-white'>
+                        Sign up
+                    </button>
+                </Link>
+            }
+          </div>
+    </div>
+  )
 }
 
 export default Home
