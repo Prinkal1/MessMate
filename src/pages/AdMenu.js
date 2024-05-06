9de0ea4ea848f27dc6c13fd54a3a15bd963fbc6a
Prinkal1 <mahajanprinkal@gmail.com>
2024-05-06 23:58:36 +0530

    admin menu added

--- src/pages/AdMenu.js
+++ src/pages/AdMenu.js
@@ -1,11 +1,34 @@
 import React from 'react'
-import {Link} from "react-router-dom"
+import Template2 from '../components/Template2'
+import today from "../assests/today.jpg"
+import week from "../assests/week.jpg"
 
-const AdMenu = ({isLoggedIn}) =>{
-    return(
-        <div>
-            <h1>menus</h1>
-        </div>
-    )
+const AdMenu = ({isLoggedIn}) => {
+  return (
+    <div className='flex flex-col items-center text-white  h-full gap-y-16 '>
+      <div className='pt-[3rem] text-6xl'>MENU-SECTION</div>
+      <div className='flex justify-center  gap-x-16 text-4xl'>
+      <Template2
+        img = {today}
+        title="TODAY'S MENU"
+        desc="Click to Check today's menu"
+        link = "/menut"
+        name = "CLICK"   
+    />      
+
+    <Template2
+        img = {week}
+        title="WEEK'S MENU"
+        desc="Check Whole Week's Menu"
+        link = "/menuw"
+        name = "CLICK"   
+    />    
+    </div>
+
+    
+
+    </div>
+  )
 }
+
 export default AdMenu
